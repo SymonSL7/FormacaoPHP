2276e501fb0644c3b5eecdd203118c8b0f72b8a4
Symon <symonlima07@gmail.com>
2025-06-17 21:06:50 -0300

    Trablhando com Banco de Dados com PDO

---
+++ 10-BancoDeDadosPDO/old-src/connect.php
@@ -0,0 +1,16 @@
+<?php
+
+    declare(strict_types=1);
+
+    $pdo = null;
+
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=exemplo', 'root', 'mysql');
+        
+    } catch (Exception $e) {
+        echo $e->getMessage();
+        die();
+    }
+
+
+    return $pdo;
