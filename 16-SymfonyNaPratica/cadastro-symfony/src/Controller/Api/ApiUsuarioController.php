69d350a0e30d2732eeaf6436811587ab9e106bac
Symon <symonlima07@gmail.com>
2025-07-01 21:42:48 -0300

    Rotas Web e API com symfony

--- 16-SymfonyNaPratica/cadastro-symfony/src/Controller/Api/ApiUsuarioController.php
+++ 16-SymfonyNaPratica/cadastro-symfony/src/Controller/Api/ApiUsuarioController.php
@@ -1,31 +1,50 @@
 <?php
 
-namespace App\Controller\Api;
+    namespace App\Controller\Api;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+    use App\Entity\Usuario;
+    use Doctrine\ORM\EntityManagerInterface;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\JsonResponse;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\Routing\Annotation\Route;
 
-#[Route("/api/v1", name: "api_v1_usuario_")]
-class ApiUsuarioController extends AbstractController
-{
-
-    #[Route("/listar", name: "listar", methods: ["GET"])]
-    public function listar(): JsonResponse
+    #[Route("/api/v1", name: "api_v1_usuario_")]
+    class ApiUsuarioController extends AbstractController
     {
 
-        return new JsonResponse(["Implementar lista na API", 404]);
+        #[Route("/listar", name: "listar", methods: ["GET"])]
+        public function listar(EntityManagerInterface $entityManager): JsonResponse
+        {
+            $repository = $entityManager->getRepository(Usuario::class);
+
+            return new JsonResponse($repository->pegarTodos(), 200);
+
+        }
+
+        #[Route("/cadastrar", name: "cadastrar", methods: ["POST"])]
+        public function cadastrar(Request $request, EntityManagerInterface $entityManager): Response
+        {
+
+            $data = $request->request->all();
+
+            $usuario = new Usuario();
+            $usuario->setNome($data['nome']);
+            $usuario->setEmail($data['email']);
+
+            $entityManager->persist($usuario);
+
+                if($usuario->getEmail() != null && $usuario->getNome() != null  && ! $usuario->getId() ) {
+
+                    $entityManager->flush();
+
+                    return new Response("ok", 200);
+
+                } else {
+                    return new Response("erro", 404);
+                }
+
+        }
 
     }
-
-    #[Route("/cadastrar", name: "cadastrar", methods: ["POST"])]
-    public function cadastrar(): JsonResponse
-    {
-
-        return new JsonResponse(["Implementar cadastro na API", 404]);
-
-    }
-
-}
--- 16-SymfonyNaPratica/cadastro-symfony/src/Controller/UsuarioController.php
+++ 16-SymfonyNaPratica/cadastro-symfony/src/Controller/UsuarioController.php
@@ -1,30 +1,52 @@
 <?php
 
-namespace App\Controller;
+    namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+    use App\Entity\Usuario;
+    use Doctrine\ORM\EntityManagerInterface;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\Routing\Annotation\Route;
 
-#[Route("/", name: "web_usuario_")]
-class UsuarioController extends AbstractController
-{
-
-    #[Route('/', name: 'index', methods: ["GET"])]
-    public function index(): Response
+    #[Route("/", name: "web_usuario_")]
+    class UsuarioController extends AbstractController
     {
 
-        return new Response("Implementar formulário de cadastro");
+        #[Route('/', name: 'index', methods: ["GET"])]
+        public function index(): Response
+        {
+
+            return $this->render("usuario/form.html.twig");
+
+        }
+
+        #[Route("/salvar", name: "salvar", methods: ["POST"])]
+        public function salvar(Request $request, EntityManagerInterface $entityManager): Response
+        {
+
+            $data = $request->request->all();
+
+            $usuario = new Usuario();
+            $usuario->setNome($data['nome']);
+            $usuario->setEmail($data['email']);
+
+            $entityManager->persist($usuario);
+
+            if($usuario->getEmail() != null && $usuario->getNome() != null  && ! $usuario->getId() ) {
+
+                $entityManager->flush();
+
+                return $this->render("usuario/sucesso.html.twig", [
+                    'fulano' => $usuario->getNome()
+                ]);
+
+            } else {
+
+                return $this->render("usuario/erro.html.twig");
+
+            }
+
+        }
 
     }
-
-    #[Route("/salvar", name: "salvar", methods: ["POST"])]
-    public function salvar(): Response
-    {
-
-        return new Response("Implementar lógica de salvar");
-
-    }
-
-}
